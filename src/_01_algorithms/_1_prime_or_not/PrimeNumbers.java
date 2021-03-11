5ceb1757096d01094512d0ed3397791458031ff4
root <root@a075445fbb22>
2021-03-11 00:50:14 +0000

    start fibonacci march 10

--- src/_01_algorithms/_1_prime_or_not/PrimeNumbers.java
+++ src/_01_algorithms/_1_prime_or_not/PrimeNumbers.java
@@ -1,9 +1,35 @@
 package _01_algorithms._1_prime_or_not;
 
+import javax.swing.JOptionPane;
+
 public class PrimeNumbers {
-public static void main(String[] args) {
-	
-}
- JOptionPane.ShowInputDialog("pick a number");
- 
-}
+	public static void main(String[] args) {
+		String answer = JOptionPane.showInputDialog("pick a number");
+		int num = Integer.parseInt(answer);
+		for (int i=2; i<num; i++) {	
+		 	JOptionPane.showMessageDialog(null, "your number is prime");
+
+		 	if (num%i==0) {
+		 	JOptionPane.showMessageDialog(null, "your number isn't prime");
+		 	}
+	}
+	}
+		
+		
+		
+		/*
+		 *
+		 * 5%2 == 3
+		 * 5%3 == 2
+		 * 5%4 == 1
+		 * 
+		 *
+		 * 6%2 == 0
+		 * 6%3 == 0
+		 * 6%4 == 2
+		 * 6%5 == 1
+		 *
+		 */
+		 
+	}
+
